50a58a3292e80f7e2933945c5d7c9261e2de7725
jtech3d <jayant62644@gmail.com>
2026-05-24 16:31:32 +0530

    complete authentication part

--- lib/apis/auth/send_otp_api.dart
+++ lib/apis/auth/send_otp_api.dart
@@ -1,29 +1,3 @@
-// //import some libraries and files
-// import 'dart:convert';
-// import 'package:frontend/models/auth/email_verification.dart';
-// import 'package:http/http.dart' as http;
-//
-// //create class
-// class SendOtpApi {
-//   //create function
-//   Future<EmailVerificationModel?> sendOtpApiFunction(String email)async{
-//     //use exception handling for handling the error
-//     try{
-//       final response = await http.post(
-//         Uri.parse("https://jtech3d.onrender.com/auth/v1/sendOtp"),
-//         headers:{"Content-Type":"application/json"},
-//         body:jsonEncode({"email":email}),
-//       );
-//       print(response.statusCode);
-//       print(response.body);
-//       final data=jsonDecode(response.body);
-//       return EmailVerificationModel.fromJson(data);
-//     }catch(e){
-//       return null;
-//     }
-//   }
-// }
-
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import '../../models/auth/email_verification.dart';
